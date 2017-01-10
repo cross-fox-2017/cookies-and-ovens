30d555839321c189c7b59ee3d0eccfe7b7f96527
broerjuang <juangwiantoro@gmail.com>
2017-01-10 14:57:31 +0700

    done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,25 @@
 //
 //
 // Your code here
+class Bake {
+  constructor(arg) {
+    this._cakeName = arg['cakeName']
+    this._bakeTime = arg['bakeTime']
+    this._random =  Math.floor(Math.random() * 4) +5
+  }
+  oven(){
+    for (var i = 0; i < 12; i++) {
+      if (i < this._random) {
+        console.log(`${this._cakeName} 'Mentah'`);
+      }else if (i == this._random){
+        console.log(`${this._cakeName} 'Matang'`);
+      }else if (i > this._random) {
+        console.log(`${this._cakeName} 'Gosong'`);
+      }
+    }
+  }
+}
+
+let masak = new Bake({cakeName: 'Chocholate Cake'})
+
+masak.oven()
