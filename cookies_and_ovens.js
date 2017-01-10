9f5353fca13de11105ab21eea717d4f7742caaf1
Your Name <name@example.com>
2017-01-10 14:31:58 +0700

    Create Class Masak

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -13,3 +13,33 @@ Menerima berbagai tipe kue, masing-masing waktu memasaknya juga berbeda
 Tambahkan status pada adonan: ‘mentah’, ‘hampir masak’, ‘selesai’, ‘hangus’.
 
 */
+class Masak {
+    constructor(params) {
+        this.namaKue = params['namaKue'];
+        this.waktuMasak = params['waktuMasak']
+    }
+
+    oven() {
+        var namaKue = this.namaKue
+        var waktuMasak = this.waktuMasak
+        var hampirMasak = this.waktuMasak - 2
+        var i = 1
+        var time = setInterval(function() {
+            if (i == 20) {
+                clearInterval(time)
+            } else {
+                if (i < hampirMasak) {
+                    console.log(`${namaKue} Masih Mentah`);
+                } else if (i < waktuMasak) {
+                    console.log(`${namaKue} Hampir Masak Siap ya`);
+                } else if (i == waktuMasak) {
+                    console.log(`${namaKue} Sudah Matang Ayo Diangkat`);
+                } else {
+                    console.log(`${namaKue} Hangus`);
+                }
+                i++
+            }
+        }, 1000)
+
+    }
+}
