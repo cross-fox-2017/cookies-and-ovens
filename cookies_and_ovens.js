e44335f6a38afc0ca6052f973945ca859b2ba98a
Wahyuhidayatt <wahyuhdyt95@gmail.com>
2017-01-10 16:52:31 +0700

    cookies_and_oven

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,57 @@
 //
 //
 // Your code here
+class Cookies{
+	constructor(waktu_mulai, waktu_matang){
+		this.waktu_mulai = waktu_mulai;
+		this.waktu_matang = 0;
+		this.status = "mentah";
+	}
+
+cook (){
+	this.waktu_mulai++
+		if(this.waktu_mulai < 8){
+			return this.status;
+		}
+		if(this.waktu_mulai > this.waktu_matang ){
+			this.status = "Hangus";
+		}
+		if(this.waktu_mulai === this.waktu_matang ){
+			this.status = "Matang";
+		}
+		if(this.waktu_mulai < this.waktu_matang){
+			this.status = "setengah mateng";
+		}
+	}
+}
+
+class KueKacang extends Cookies{
+	constructor(waktu_mulai, waktu_matang){
+		super(waktu_mulai)
+		this.waktu_matang = 10
+	}
+}
+class KueKeju extends Cookies{
+	constructor(waktu_mulai, waktu_matang){
+		super(waktu_mulai)
+		this.waktu_matang = 10
+	}
+}
+var kue = new KueKeju(3)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+kue.cook()
+console.log(kue)
+
