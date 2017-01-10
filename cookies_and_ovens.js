017327ed596c1a11ca311d6216b80b1d4c5c7451
Raditya <arya.raditya26@gmail.com>
2017-01-10 14:35:41 +0700

    done

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,68 @@
 //
 //
 // Your code here
+"use strict"
+
+class Cookies {
+  constructor(waktu) {
+    this.waktu = waktu
+    this.status = 'mentah'
+    this.waktuMatang = 0
+  }
+
+  cook(){
+    this.waktu++
+    //console.log(this.waktuMatang);
+    if (this.waktu >= 6) {
+      this.status = 'setengah matang'
+    }
+    if (this.waktu >= 8) {
+      this.status = 'hampir matang'
+    }
+    if (this.waktu === this.waktuMatang) {
+      this.status = 'matang!'
+    }
+    if (this.waktu > this.waktuMatang) {
+      this.status = 'hangus!'
+    }
+  }
+}
+
+class KueKacang extends Cookies {
+  constructor(waktu, waktuMatang) {
+    super(waktu, waktuMatang)
+    this.waktu = waktu
+    this.waktuMatang = 10
+  }
+}
+
+class KueCoklat extends Cookies {
+  constructor(waktu) {
+    super(waktu)
+    this.waktuMatang = 12
+  }
+}
+
+class KueJahe extends Cookies {
+  constructor(waktu) {
+    super(waktu)
+    this.waktuMatang = 15
+  }
+}
+
+// Driver code ..
+let kue = new KueKacang(5)
+console.log(kue);
+
+kue.cook()
+console.log(kue);
+kue.cook()
+console.log(kue);
+kue.cook()
+console.log(kue);
+kue.cook()
+console.log(kue);
+kue.cook()
+console.log(kue);
+kue.cook()
+console.log(kue);
