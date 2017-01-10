8d3d803d15e9d55103d0a77e8dfd819ff0763aaa
pisanggoreng <7ofpentacles@gmail.com>
2017-01-11 01:15:23 +0700

    clear

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,61 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+class Cookie {
+  constructor(nama, waktuMasak) {
+    this.nama = nama
+    this.waktuMasak = waktuMasak
+    this.status = 'mentah'
+    this.time = 0
+  }
+
+  masak(waktu) {
+    this.time += waktu
+    if (this.waktuMasak > this.time) {
+      this.status = "setengah matang"
+    } else if (this.waktuMasak == this.time) {
+      this.status = "sudah matang"
+    } else if (this.waktuMasak < this.time) {
+      this.status = "gosong"
+    }
+  }
+
+  printStatus() {
+    console.log( `${this.nama} sudah dimasak selamat ${this.time} dan hasilnya ${this.status}` );
+  }
+}
+
+class KueKacang extends Cookie {
+  constructor(nama, waktuMasak) {
+    super(nama, waktuMasak)
+  }
+}
+
+class KueCoklat extends Cookie {
+  constructor(nama, waktuMasak) {
+    super(nama, waktuMasak)
+  }
+}
+
+class Oven {
+  constructor() {
+    this.cookies = []
+  }
+
+  tambahKue(nama, waktuMasak) {
+    if (nama == "Kue Kacang") {
+      this.cookies.push( new KueKacang(nama, waktuMasak) )
+    } else if ( nama == "Kue Coklat" ) {
+      this.cookies.push( new KueCoklat(nama, waktuMasak) )
+    }
+
+  }
+}
+
+var kue = new Oven();
+kue.tambahKue("Kue Kacang", 15);
+kue.tambahKue("Kue Coklat", 20);
+kue.cookies[1].masak(5);
+kue.cookies[1].masak(5);
+kue.cookies[1].masak(5);
+kue.cookies[1].masak(5);
+kue.cookies[1].masak(5);
+kue.cookies[1].printStatus()
+// kue.tambahKue[0];
