a1e4fd1cdf312f26c5348d741a30e1248f40fd0c
Dale <dellryuzi@gmail.com>
2017-01-10 13:34:46 +0700

    input nama cookie + wktu pembuatan

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,60 @@
 //
 //
 // Your code here
+class Cook{
+  constructor(rasa,waktuPembuatan){
+    // this.tepung = tepung;
+    this.rasa = rasa;
+    this.waktuPembuatan = waktuPembuatan;
+
+  }
+  setCookie(newRasa,newWaktu){
+    this.rasa =newRasa;
+    this.newWaktu = newWaktu;
+  }
+  getCookie(){
+    return this.rasa,this.waktuPembuatan;
+    // return this.rasa
+  }
+}
+
+class Oven{
+  constructor(cookie,time = 0,level){
+    this.cookie = cookie;
+    this.time = time;
+    this.level = level;
+
+  }
+  cookCookie(str,lamacookie){
+    var c = 0;
+
+    while(c<=98){
+      this.cookie = new Cook(str,lamacookie);
+
+      if( this.time == this.cookie["waktuPembuatan"] ){
+      //  this.time = this.cookie["waktuPembuatan"] ;
+       this.level = "welldone"
+       c = 99 ;
+       console.log(`Oven time = ${this.time +1}mins , Cookie : ${this.cookie["rasa"]},level kematangan ${this.level}`)
+       console.log(this.cookie);
+       console.log("\n")
+
+       this.time = 0;
+      }else {
+        this.time+= 1;
+        this.level = "medium"
+        console.log(`Oven time = ${this.time}mins , Cookie : ${this.cookie["rasa"]},level kematangan ${this.level}`)
+        c++;
+      }
+    }
+  }
+
+// if((this.time < this.cookie["waktuPembuatan"]))
+}
+
+var makeCookie = new Oven();
+
+//name cookie , waktu pembuatan
+makeCookie.cookCookie("peanut",3)
+
+makeCookie.cookCookie("cheese",10)
