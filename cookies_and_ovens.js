7863925322cfe8977275d54e02ecfe2619f20f01
Your Name <name@example.com>
2017-01-10 14:31:16 +0700

    Menulis Kebutuhan Program

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -1,10 +1,15 @@
-// Answer These Questions:
-//
-// - What are essential classes?
-// - What attributes will each class have?
-// - What interface will each class provide?
-// - How will the classes interact with each other?
-// - Which classes will inherit from others, if any?
-//
-//
-// Your code here
+/*
+Sebagai tukang masak, saya ingin membuat berbagai jenis kue (kue kacang, coklat dan lain sebagainya.)
+Sebagai tukang masak, saya ingin menumpuk adonan kue ke dalam oven.
+Sebagai tukang masak, saya ingin tahu kapan harus mengangkat kue saya dari oven biar tidak hangus.
+
+Apa saja class yang perlu dibuat?
+Apa saja properti yang dipunyai setiap class?
+Apa saja interface yang perlu ada pada tiap class?
+Bagaimana class berinteraksi dengan class lainnya?
+Class mana yang merupakan inheritance, jika ada?
+
+Menerima berbagai tipe kue, masing-masing waktu memasaknya juga berbeda
+Tambahkan status pada adonan: ‘mentah’, ‘hampir masak’, ‘selesai’, ‘hangus’.
+
+*/
