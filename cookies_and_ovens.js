c724c9109173c363f24200d2f237a9b3979895a4
irwinpratajaya <irwinpratajaya@gmail.com>
2017-01-10 16:20:03 +0700

    cookies

--- cookies_and_ovens.js
+++ cookies_and_ovens.js
@@ -8,3 +8,63 @@
 //
 //
 // Your code here
+class Cookies {
+  constructor(nama, waktuMasak) {
+    this.nama = nama;
+    this.waktu = 0;
+    this.waktuMasak = waktuMasak;
+    this.status = 'mentah'
+  }
+
+  masak() {
+    this.waktu += 5
+    if (this.waktuMasak > this.waktu) {
+      this.status = 'hampir masak'
+    } else if (this.waktuMasak == this.waktu) {
+      this.status = 'matang'
+    } else if (this.waktuMasak < this.waktu ) {
+      this.status = 'gosong'
+    }
+  }
+
+  printStatus() {
+    console.log(`${this.nama} telah dimasak selama ${this.waktu} menit dan statusnya adalah ${this.status}`);
+  }
+
+}
+
+class KueKacang extends Cookies {
+  constructor() {
+    super("Kue Kacang", 20)
+  }
+}
+
+class KueCoklat extends Cookies {
+  constructor() {
+    super("Kue Coklat", 30)
+  }
+}
+
+var kue1 = new KueKacang()
+var kue2 = new KueCoklat()
+console.log(kue1);
+console.log(kue2);
+
+kue1.masak()
+kue2.masak()
+kue1.printStatus()
+kue1.masak()
+kue2.masak()
+kue1.printStatus()
+kue1.masak()
+kue2.masak()
+kue1.printStatus()
+kue1.masak()
+kue2.masak()
+kue2.printStatus()
+kue1.masak()
+kue2.masak()
+kue1.printStatus()
+kue1.masak()
+kue2.masak()
+kue2.printStatus()
